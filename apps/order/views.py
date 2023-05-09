27c59ed9ce4f13a9654276982994c644c1b36a63
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-09 16:56:09 +0300

    edited: optimize views
     - available_tables
     - occupancy
     - create

--- apps/order/views.py
+++ apps/order/views.py
@@ -1,6 +1,5 @@
 from django.core.mail import send_mail
-from django.db.models import F, Q, Sum
-from django.db.models import F, Q, Sum
+from django.db.models import F, Sum
 from django.shortcuts import get_object_or_404
 from django.utils.dateparse import parse_date
 from rest_framework import status, viewsets
@@ -26,7 +25,7 @@ class TableViewSet(viewsets.ModelViewSet):
         Return a list of reserved dates for a specific table.
         """
         table = get_object_or_404(Table, pk=pk)
-        reserved_dates = Order.objects.filter(table=table).values_list("date", flat=True)
+        reserved_dates = table.orders.values_list("date", flat=True)
         return Response(reserved_dates)
 
     @action(detail=False, methods=["get"])
@@ -38,9 +37,7 @@ class TableViewSet(viewsets.ModelViewSet):
         if not date_str:
             return Response([], status=status.HTTP_404_NOT_FOUND)
         else:
-            available_tables = Table.objects.exclude(
-                orders__date=parse_date(date_str)
-            )
+            available_tables = Table.objects.exclude(orders__date=parse_date(date_str))
             serializer = TableSerializer(available_tables, many=True)
             return Response(serializer.data)
 
@@ -50,12 +47,12 @@ class TableViewSet(viewsets.ModelViewSet):
         Return the occupancy percentage for a specific date.
         """
         date_str = request.GET.get("date")
-        if date_str:
-            date = parse_date(date_str)
-            reserved_tables = Order.objects.filter(date=date).values_list("table", flat=True)
+        if not date_str:
+            return Response([], status=status.HTTP_404_NOT_FOUND)
+        else:
             total_area = Table.objects.aggregate(total_area=Sum(F("width") * F("length")))["total_area"]
-            occupied_area = Order.objects.filter(id__in=reserved_tables).aggregate(
-                occupied_area=Sum(F("table__width") * F("table__length"))
+            occupied_area = Table.objects.exclude(orders__date=parse_date(date_str)).aggregate(
+                occupied_area=Sum(F("width") * F("length"))
             )["occupied_area"]
             occupancy_percentage = round(occupied_area / total_area * 100, 2)
             return Response(
@@ -65,8 +62,6 @@ class TableViewSet(viewsets.ModelViewSet):
                     "occupied_area": occupied_area,
                 }
             )
-        else:
-            return Response([], status=status.HTTP_404_NOT_FOUND)
 
 
 class OrderViewSet(viewsets.ModelViewSet):
@@ -82,9 +77,11 @@ class OrderViewSet(viewsets.ModelViewSet):
         Create a new order and send a confirmation email to the customer.
         """
         table_id = request.data.get("table_id")
-        if table_id:
+        if not table_id:
+            return Response([], status=status.HTTP_404_NOT_FOUND)
+        else:
             table = get_object_or_404(Table, id=table_id)
-            if Order.objects.filter(table=table, date=request.data.get("date")).exists():
+            if table.orders.filter(date=request.data.get("date")).exists():
                 return Response(
                     {"error": "This table is already booked on this date"},
                     status=status.HTTP_400_BAD_REQUEST,
@@ -103,5 +100,3 @@ class OrderViewSet(viewsets.ModelViewSet):
                 fail_silently=False,
             )
             return Response(serializer.data, status=status.HTTP_201_CREATED, headers=headers)
-        else:
-            return Response([], status=status.HTTP_404_NOT_FOUND)
