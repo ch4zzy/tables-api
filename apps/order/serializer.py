fcd0f79f42425d30c4c7cdadb6a4e4c03a78061b
Dmytro Kolesnyk <kolesnik.dmitry16.1@gmail.com>
2023-05-11 16:14:09 +0300

    edited: added few validators, new serializer
     - Validators:
       - validate_date
       - validate_format
     - Serializer:
       - AvailableTableSerializer without customer data fields:
         - customer_email
         - customer_name

--- apps/order/serializer.py
+++ apps/order/serializer.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
+from django.utils import timezone
 from rest_framework import serializers
+from rest_framework.validators import ValidationError
 
 from apps.order.models import Order, Table
 
@@ -10,6 +14,34 @@ class TableSerializer(serializers.ModelSerializer):
 
 
 class OrderSerializer(serializers.ModelSerializer):
+    date = serializers.DateField(format="%d-%m-%Y", input_formats=["%d-%m-%Y"])
+
+    def validate_format(self, value):
+        """
+        Validation date format
+        """
+        if value != datetime.strptime(value, "%d-%m-%Y").date():
+            raise ValidationError("Invalid date format. Please use the format 'dd-mm-yyyy'.")
+        return value
+
+    def validate_date(self, value):
+        """
+        Validation date format
+        """
+        if value < timezone.now().date():
+            raise ValidationError("Date cannot be in the past.")
+        return value
+
     class Meta:
         model = Order
         fields = ["id", "date", "customer_name", "customer_email"]
+
+
+class AvailableTablesSerializer(OrderSerializer):
+    class Meta(OrderSerializer.Meta):
+        fields = ["id", "date"]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields.pop("customer_name", None)
+        self.fields.pop("customer_email", None)
